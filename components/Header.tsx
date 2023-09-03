7b5f665661449e976166ba2ed2be7fa1e074931d
Rudra <patelrudra.3000@gmail.com>
2023-09-03 11:25:30 +0530

    Added React-Avatar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from 'next/image'
 import React from 'react'
 import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import Avatar from 'react-avatar'
 
 function Header() {
   return (
@@ -14,7 +17,7 @@ function Header() {
             className='w-44 md:w-56 pb-10 md:pb-0 object-contain cursor-pointer'
         />
 
-        <div>
+        <div className='flex items-center w-full space-x-5 flex-1 justify-end'>
             <form className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
                 <MagnifyingGlassIcon className='h-6 w-6 text-gray-400' />
                 <input 
@@ -24,7 +27,16 @@ function Header() {
                 />
                 <button className="hidden">Search</button>
             </form>
+
+            <Avatar name='Rudra Patel' round size='50' color='#0055D1' />
         </div>
+      </div>
+
+      <div className='flex items-center justify-center px-5 md:py-5'>
+        <p className='flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]'>
+          <UserCircleIcon className='inline-block h-10 w-10 text-[#0055D1] mr-1' />
+          GPT is summarising your tasks for the day... 
+        </p>
       </div>
     </header>
   )
